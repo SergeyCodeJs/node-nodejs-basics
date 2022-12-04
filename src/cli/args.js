0b757a8d979ab120d584d13515bdfbca3189688e
SergeyCodeJs <sergeysteh@gmail.com>
2022-12-04 14:04:55 +0100

    feat: add cli functions implementation

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
+
+/**
+ * Function that parses command line arguments (given in format --propName value --prop2Name 
+ * value2, you don't need to validate it) and prints them to the console in the format propName is
+ * value, prop2Name is value2
+ */
 const parseArgs = () => {
-    // Write your code here 
+    const argv = [...process.argv];
+    argv.forEach((arg, idx) => {
+        if (idx > 1) {
+            console.log(`prop${idx - 1}Name is ${arg}`);
+        }
+    });
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+
+
+
+/**
+ * Function that parses environment variables with prefix RSS_ and prints them to the console in 
+ * the format RSS_name1=value1; RSS_name2=value2
+ */
 const parseEnv = () => {
-    // Write your code here 
+    const envs = Object.entries(process.env).map((env) => {
+        return `RSS_${env[0]}=${env[1]}`;
+    });
+    console.log(envs);
 };
 
 parseEnv();
