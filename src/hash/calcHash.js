c21b1e73a6103661db94ad336fb5646ee88ff72b
SergeyCodeJs <sergeysteh@gmail.com>
2022-12-04 16:32:55 +0100

    feat: add new calcHash file with solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import * as fs from 'fs';
+import * as crypto from 'crypto';
+import * as path from 'path';
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/**
+ * implement function that calculates SHA256 hash for file fileToCalculateHashFor.txt and logs it
+ * into console as hex
+ */
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, './files', 'fileToCalculateHashFor.txt');
+    const fileBuffer = fs.readFileSync(filePath);
+    const hashSum = crypto.createHash('sha256');
+    hashSum.update(fileBuffer);
+    const hex = hashSum.digest('hex');
+    console.log(hex);
 };
 
 await calculateHash();
