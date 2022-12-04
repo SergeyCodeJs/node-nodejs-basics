542f98a7419d1bbbd6106b3b4a645630e23aa74f
SergeyCodeJs <sergeysteh@gmail.com>
2022-12-04 16:56:40 +0100

    feat: add zip implementations

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import * as zlib from 'zlib';
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/**
+ * Function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API
+ */
 const compress = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, './files', 'fileToCompress.txt');
+    const outputPath = path.join(__dirname, './files', 'archive.gz');
+    const isFileToCompress = fs.existsSync(filePath);
+    if (isFileToCompress) {
+        const zip = zlib.createGzip();
+        const read = fs.createReadStream(filePath);
+        const write = fs.createWriteStream(outputPath);
+        read.pipe(zip).pipe(write);
+        console.log("Zipped Successfully");
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,27 @@
+import * as zlib from 'zlib';
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/**
+ * function that decompresses archive.gz back to the fileToCompress.txt with same content as
+ * before compression using zlib and Streams API
+ */
+
 const decompress = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, './files', 'archive.gz');
+    const outputPath = path.join(__dirname, './files', 'fileToCompress.txt');
+    const isFileToDecompress = fs.existsSync(filePath);
+    if (isFileToDecompress) {
+        const unzip = zlib.createGunzip();
+        const read = fs.createReadStream(filePath);
+        const write = fs.createWriteStream(outputPath);
+        read.pipe(unzip).pipe(write);
+        console.log("Unzipped Successfully");
+    }
 };
 
 await decompress();
