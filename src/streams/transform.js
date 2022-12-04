001c0c288d560748ce60214ce123f543e7b3b87d
SergeyCodeJs <sergeysteh@gmail.com>
2022-12-04 16:33:35 +0100

    feat: add streams solutions

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform } from 'stream';
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+
+/**
+ * Function that reads data from process.stdin, reverses text using Transform Stream and then
+ * writes it into process.stdout
+ */
 const transform = async () => {
-    // Write your code here 
+    const reverse = new Transform({
+        transform(chunk, encoding, callback) {
+            const string = chunk.toString().split('').reverse().join('');
+            callback(null, string);
+        }
+    });
+    process.stdin.pipe(reverse).pipe(process.stdout);
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/**
+ *  Function that writes process.stdin data into file fileToWrite.txt content using Writable Stream
+ */
 const write = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, './files', 'fileToWrite.txt');
+    const writableStream = fs.createWriteStream(filePath, 'utf-8');
+    process.stdin.on('data', data => {
+        writableStream.write(data, (err) => {
+            if (err) throw new Error(err);
+            console.log(`Data ${data} is written into file!`);
+        });
+    });
 };
 
 await write();
