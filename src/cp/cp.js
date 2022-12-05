5e5910b5e2d1aeaa9b6684d5dff09967aad634d1
SergeyCodeJs <sergeysteh@gmail.com>
2022-12-05 21:12:05 +0100

    feat: add cp implementation

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,24 @@
+import { spawn, exec, fork, execFile } from 'child_process'
+import * as fs from 'fs'
+import * as path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/**
+ * Function that receives array of arguments args and creates child
+ * process from file script.js, passing these args to it. This function should create IPC-channel
+ * between stdin and stdout of master process and child process:
+ * child process stdin should receive input from master process stdin
+ * child process stdout should send data to master process stdout
+ */
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const scriptPath = path.join(__dirname, './files', 'script.js');
+    const forked = fork(scriptPath, args);
+    forked.on('message', (msg) => {
+        console.log(msg);
+    });
 };
 
-spawnChildProcess();
+spawnChildProcess(['argument1', 'argument2']);
--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -6,7 +6,8 @@ console.log(`Arguments: ${JSON.stringify(args)}`);
 const echoInput = (chunk) => {
     const chunkStringified = chunk.toString();
     if (chunkStringified.includes('CLOSE')) process.exit(0);
-    process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
+    process.stdout.write(`Received from master process: ${chunk.toString()}\n`);
+    process.send('I am master process and that is an answer from child process');
 };
 
 process.stdin.on('data', echoInput);
